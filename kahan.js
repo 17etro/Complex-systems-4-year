304e854f77d67b30bb92534820defe570da94ed2
17etro <petrpetryk2016@gmail.com>
2023-03-19 14:36:32 +0200

    implement kahan algorithm

---
+++ kahan.js
@@ -0,0 +1,21 @@
+function kahanSum(inputArray) {
+  let sum = 0.0;
+  let c = 0.0;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    y = inputArray[i] - c;
+    t = sum + y;
+    c = (t - sum) - y;
+    sum = t;
+  }
+  
+  return sum
+}
+
+// -tests
+
+// result --> 0.6000000000000001
+console.log(0.1 + 0.2 + 0.3);
+
+// result --> 0.6
+console.log(kahanSum([0.1, 0.2, 0.3]))
